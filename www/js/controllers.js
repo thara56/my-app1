e7ae4f05495cb02a5091fc5e9fdfea8e9797375e
thara56 <tharaka.wijendra@gmail.com>
2015-01-26 17:56:59 +1100

    Added the clock

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $interval) {
+   var _i, _results;
+  $scope.date = new Date();
+  $scope.hourMarkers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  $scope.minuteMarkers = (function() {
+    _results = [];
+    for (_i = 0; _i <= 59; _i++){ _results.push(_i); }
+    return _results;
+  }).apply(this);
+  $scope.getDegreesForUnit = function(unit, totalUnits) {
+    return (360 / totalUnits) * unit;
+  };
+  $scope.getHours = function() {
+    return $scope.getDegreesForUnit($scope.date.getHours() + ($scope.date.getMinutes() / 60), 12);
+  };
+  $scope.getMinutes = function() {
+    return $scope.getDegreesForUnit($scope.date.getMinutes() + ($scope.date.getSeconds() / 60), 60);
+  };
+  $scope.getSeconds = function() {
+    return $scope.getDegreesForUnit($scope.date.getSeconds() + ($scope.date.getMilliseconds() / 1000), 60);
+  };
+  return $interval(function() {
+    return $scope.date = new Date();
+  }, 1000 / 30);
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
